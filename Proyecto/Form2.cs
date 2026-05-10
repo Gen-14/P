1ee7b57f8f62c6c58bf95c013d96bdc5add78d62
cris0309 <crisleiper2@gmail.com>
2026-05-10 14:56:50 -0600

    Cambios en form2

--- Proyecto/Form2.cs
+++ Proyecto/Form2.cs
@@ -7,66 +7,123 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Proyecto
 {
     public partial class pedidos : Form
     {
-       
+
         public pedidos()
         {
             InitializeComponent();
-            cargarDatos();
         }
         //carga los pedidos
+        private void pedidos_Load(object sender, EventArgs e)
+        {
+            cargaDatosTxt();
+        }
         public void cargarDatos()
         {
             dataGridView1.DataSource = null;
             dataGridView1.DataSource = Datos.ListaPedidos;
+            dataGridView1.Refresh();
         }
         //agregar los pedidos
         public void bAgregar_Click(object sender, EventArgs e)
         {
-            pedido nuevo = new pedido()
-            {
-                id = Datos.ListaPedidos.Count + 1,
-                fecha = dtFecha.Value,
-                cliente = tbCliente.Text,
-                productos = tbProductos.Text,
-                estado = estado.Text
-            };
-            Datos.ListaPedidos.Add(nuevo);
+            int nuevoID = 1;
 
-            //carga los datos ingresados en la tabla y muestra un mensaje
-            limpiarCampos();
-            cargarDatos();
-            MessageBox.Show("Pedido agregado correctamente");
+            try
+            {
+                if (string.IsNullOrWhiteSpace(tbCliente.Text) || string.IsNullOrWhiteSpace(tbProductos.Text))
+                //string.IsNullOrWhiteSpace(txtCantidad.Text))
+                {
+                    MessageBox.Show("Debe completar todos los campos");
+                    return;
+                }
+                if (Datos.ListaPedidos.Count > 0)
+                {
+                    nuevoID = Datos.ListaPedidos.Max(p => p.id) + 1;
+                }
+                pedido nuevo = new pedido()
+                {
+                    id = nuevoID,
+                    fecha = dtFecha.Value,
+                    cliente = tbCliente.Text,
+                    productos = tbProductos.Text,
+                    estado = estado.Text
+                };
+                Datos.ListaPedidos.Add(nuevo);
+                //carga los datos ingresados en la tabla y muestra un mensaje
+                //Hacer el archivo de txt
+                pedido.archivoTxt();
+                cargarDatos();
+                limpiarCampos();
+
+                MessageBox.Show("Pedido agregado correctamente");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocurrió un error: " + ex.Message);
+            }
+
         }
         //metodo que limpia los campos de texto 
         private void limpiarCampos()
         {
             tbCliente.Text = "";
             tbProductos.Text = "";
-            estado.Text = "";
             dtFecha.Value = DateTime.Now;
         }
+        public void cargaDatosTxt()
+        {
+            Datos.ListaPedidos.Clear();
+            //cargar los datos del archivo txt
+
+            if (File.Exists("pedidos.txt"))
+            {
+                foreach (string linea in File.ReadAllLines("pedidos.txt"))
+                {
+                    if (string.IsNullOrWhiteSpace(linea)) continue;
+
+                    string[] datos = linea.Split(',');
+
+                    if (datos.Length == 5)
+                    {
+                        pedido nuevo = new pedido()
+                        {
+                            id = int.Parse(datos[0]),
+                            fecha = DateTime.Parse(datos[1]),
+                            cliente = datos[2],
+                            productos = datos[3],
+                            estado = datos[4]
+                        };
+
+                        Datos.ListaPedidos.Add(nuevo);
+                    }
+                }
+            }
+            cargarDatos();
+        }
+
         //botones que abren otros formularios (menu)
         private void menúToolStripMenuItem_Click(object sender, EventArgs e)
         {
             menu nuevoForm = new menu();
             nuevoForm.Show();
-            this.Hide();
+            this.Close();
         }
         private void verPedidosToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            Form3 nuevoForm = new Form3();
+            verPedidos nuevoForm = new verPedidos();
             nuevoForm.Show();
-            this.Hide();
+            this.Close();
         }
 
-        private void pedidos_Load(object sender, EventArgs e)
+        private void pedidos_FormClosing(object sender, FormClosingEventArgs e)
         {
-            cargarDatos();
+            Application.Exit();
         }
     }
 }
